3e1c9b3ce54583a21c93181f7466ccf2cfe62ad7
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2022-04-29 02:16:23 -0400

    Send  movies from DB
    Add template of greetings and farewells

--- main.py
+++ main.py
@@ -1,14 +1,67 @@
 import os
+import requests
+import numpy as np
+import pandas as pd
+import pyodbc
 import telebot
+import nltk
+import string
+import random
+from nltk.corpus import stopwords
 from dotenv import load_dotenv
 from telebot.types import ReplyKeyboardMarkup, ForceReply
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 USER_DATA = {}
 load_dotenv()
 BOT_KEY = os.getenv('BOT_KEY')
+IMDB_API_KEY = os.getenv('IMDB_API_KEY')
 EMAIL_ADDERS = os.getenv('EMAIL_ADDERS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 bot = telebot.TeleBot(BOT_KEY)
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=JRODRIGUEZDIAZZ\SQLEXPRESS;'
+                      'Database=store;'
+                      'Trusted_Connection=yes;')
+cursor = conn.cursor()
+
+SALUDOS_INPUTS = (
+    "hola", "buenas", "saludos", "qué tal", "hey", "buenos dias", "klk", "buenas tardes", "buenas noches", "dime a ver")
+
+SALUDOS_OUTPUTS = ["ROBOT: Hola", "ROBOT: Hola, ¿Qué tal?", "ROBOT Hola, ¿Cómo te puedo ayudar?",
+                   "ROBOT: Hola, encantado de hablar contigo", "ROBOT: Buenas, ¿Cómo le puedo servir?", "ROBOT: klk",
+                   "ROBOT: Dime a ver", "ROBOT: ¿En qué te puedo ayudar?"]
+
+DESPEDIDA_OUTPUTS = ["ROBOT: No hay de qué", "ROBOT: Con mucho gusto", "ROBOT: De nada", "ROBOT: Le estaré esperando",
+                     "ROBOT: Vuelva pronto"]
+
+
+def getLemerTokens():
+    file = open(r'store.txt', 'r', errors='ignore')
+    raw = file.read()
+    raw = raw.lower()
+    sent_tokens = nltk.sent_tokenize(raw)
+    word_tokens = nltk.word_tokenize(raw)
+    lemmer = nltk.stem.WordNetLemmatizer()
+    return lemmer
+
+
+def LemTokens(tokens):
+    lemmer = getLemerTokens()
+    return [lemmer.lemmatize(token) for token in tokens]
+
+
+def LemNormalize(text):
+    remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+def saludos(sentence):
+    for word in sentence.split():
+        if word.lower() in SALUDOS_INPUTS:
+            return random.choice(SALUDOS_OUTPUTS)
 
 
 @bot.message_handler(["help", "start"])
@@ -53,11 +106,32 @@ def save_user_data(message):
     bot.send_message(message.chat.id, text, parse_mode="html")
 
 
+def get_photo(title):
+    url = "https://imdb-api.com/en/API/SearchMovie/{IMDB_API_KEY}/{title}".format(IMDB_API_KEY=IMDB_API_KEY,
+                                                                                  title=title)
+    response = requests.get(url)
+    data = response.json()
+    image = data["results"][0]["image"]
+    return image
+
+
+@bot.message_handler(commands=["get_movies"])
+def get_movies(message):
+    query = "SELECT TOP 2 * FROM movies ORDER BY id;"
+    movies = pd.read_sql_query(query, conn)
+    for index, row in movies.iterrows():
+        text = f'Información sobre {row["title"]}: \n'
+        text += f'<b>Fecha de estreno: {row["release_date"]}</b>\n'
+        text += f'<b>Precio: ${row["price"]}</b>\n'
+
+        bot.send_message(message.chat.id, text, parse_mode="html")
+
+
 if __name__ == '__main__':
     print("Welcome to the bot")
     bot.set_my_commands([
         telebot.types.BotCommand(command="/start", description="Iniciar el bot"),
-        telebot.types.BotCommand(command="/ask", description="Empezar a preguntar"),
+        telebot.types.BotCommand(command="/get_movies", description="Obtener la lista de películas")
     ])
     bot.infinity_polling()
     print("Goodbye")
