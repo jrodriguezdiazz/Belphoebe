f07e4947cbaea45f867bb8224bd64192c628641f
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2022-04-29 00:33:55 -0400

    Add functionality to capture data that the user answers
    The user is asked for their name, phone number and gender

--- main.py
+++ main.py
@@ -1,65 +1,63 @@
 import os
 import telebot
-import smtplib
-from telebot import types
 from dotenv import load_dotenv
+from telebot.types import ReplyKeyboardMarkup, ForceReply
 
+USER_DATA = {}
 load_dotenv()
 BOT_KEY = os.getenv('BOT_KEY')
 EMAIL_ADDERS = os.getenv('EMAIL_ADDERS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 bot = telebot.TeleBot(BOT_KEY)
 
-commands = ["help", "start"]
 
-
-@bot.message_handler(commands)
+@bot.message_handler(["help", "start"])
 def send_message(message):
     bot.reply_to(message, "Hello, How are you? 🙋🏻‍♀️")
 
 
-def get_product_information():
-    return "SELECT * FROM products"
+@bot.message_handler(commands=["ask"])
+def start_ask(message):
+    markup = ReplyKeyboardMarkup()
+    msg = bot.reply_to(message, "What is your name?", reply_markup=markup)
+    bot.register_next_step_handler(msg, ask_phone_number)
 
 
-def get_order_information():
-    return "SELECT * FROM products"
+def ask_phone_number(message):
+    USER_DATA[message.chat.id] = {}
+    USER_DATA[message.chat.id]["name"] = message.text
+    markup = ForceReply()
+    msg = bot.send_message(message.chat.id, "¿Cuál es tu número telefónico?", reply_markup=markup)
+    bot.register_next_step_handler(msg, ask_sex)
 
 
-def get_recommendations():
-    return []
+def ask_sex(message):
+    if not message.text.isdigit():
+        markup = ForceReply()
+        msg = bot.send_message(message.chat.id, "¿Cuál es tu número telefónico?", reply_markup=markup)
+        bot.register_next_step_handler(msg, ask_sex)
+    else:
+        USER_DATA[message.chat.id]["phone"] = int(message.text)
+        markup = ReplyKeyboardMarkup(one_time_keyboard=True, input_field_placeholder="Pulsa el botón para enviar")
+        markup.add("Hombre", "Mujer")
+        msg = bot.send_message(message.chat.id, "¿Eres hombre o mujer?", reply_markup=markup)
+        bot.register_next_step_handler(msg, save_user_data)
 
 
-def send_email(to_adders):
-    order = 'A111'
-    message = 'Subject: Prueba \n\n Orden #{order} registrada. (Estará disponible en un lapso no mayor a 7 días.)'.format(
-        order=order)
-    connection = smtplib.SMTP('smtp.gmail.com', 587)
-    connection.ehlo()
-    connection.starttls()
-    connection.login(EMAIL_ADDERS, EMAIL_ADDERS)
-    connection.sendmail(EMAIL_ADDERS, to_adders, message)
+def save_user_data(message):
+    USER_DATA[message.chat.id]["sex"] = message.text
+    text = "Datos Introducidos: \n"
+    text += f'<code>Nombre: {USER_DATA[message.chat.id]["name"]}</code>\n'
+    text += f'<code>Número telefónico: {USER_DATA[message.chat.id]["phone"]}</code>\n'
+    text += f'<code>Sexo: {USER_DATA[message.chat.id]["sex"]}</code>\n'
+    bot.send_message(message.chat.id, text, parse_mode="html")
 
 
-def reserve_product():
-    query = ""
-
-
-def delete_order():
-    query = ""
-
-
-def show_menu(message):
-    chat_id = message.chat.id
-    first_name = message.chat.first_name
-    message = "Elija la opción deseada {first_name}?".format(first_name=first_name)
-    prod_btn = types.ReplyKeyboardMarkup(row_width=1)
-    new_order = types.KeyboardButton("ORDENAR")
-    consult_order = types.KeyboardButton("CONSULTAR ORDEN")
-    cancel_order = types.KeyboardButton("CANCELAR ORDEN")
-    prod_btn.row(new_order, consult_order)
-    prod_btn.row(cancel_order)
-    bot.send_message(chat_id, message, reply_markup=prod_btn)
-
-
-bot.polling()
+if __name__ == '__main__':
+    print("Welcome to the bot")
+    bot.set_my_commands([
+        telebot.types.BotCommand(command="/start", description="Iniciar el bot"),
+        telebot.types.BotCommand(command="/ask", description="Empezar a preguntar"),
+    ])
+    bot.infinity_polling()
+    print("Goodbye")
